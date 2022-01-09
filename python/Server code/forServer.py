1d5b9c91a78ed42776a287802f71ea6a5525d1bc
JoeHalasz <jhalasz@yahoo.com>
2022-01-09 17:42:14 -0500

    time stamps and removed useless prints and comments
    also prints pc list if there is no good PC to connect to and connects to PC no matter the name if there is only one

--- python/Server code/forServer.py
+++ python/Server code/forServer.py
@@ -7,14 +7,13 @@ import traceback
 
 connectedPCs = []
 connectedPIs = []
-
+serverStartTime = time.time()
 
 # this will get the PI's id or create one for it if it doesnt have one, and send it back to the pi
 def getID(connection):
 	data = connection.recv(1024) # should be just an ID
 	if data:
 		data = data.decode('utf-8')
-		print(data)
 		if data == "-1":
 			piId = str(firstTimeConnection())
 		else:
@@ -22,26 +21,6 @@ def getID(connection):
 
 		connection.send(piId.encode('utf-8'))
 		return piId
-
-
-# def handlePC(connection):
-# 	global connectedPCs
-# 	global connectedPIs
-# 	connection.settimeout(5) # 5 seconds
-# 	try:
-# 		while True:
-# 			print("sending")
-# 			connection.send("connected?".encode('utf-8'))
-# 			print("waiting for recv")
-# 			data = connection.recv(1024)
-# 			print(connectedPCs)
-# 	except: # this means that it disconnected
-# 		print("disconnected")
-# 		for i in range(len(connectedPCs)): # remove it from the connected lists
-# 			c = connectedPCs[i]
-# 			if c[0] == connection:
-# 				connectedPCs.pop(i)
-# 	print(connectedPCs)
 
 
 def handlePI(connection, piAddr):
@@ -55,50 +34,51 @@ def handlePI(connection, piAddr):
 			strlen = connection.recv(8).decode("utf-8")
 			length = int(strlen) - 10000000 # added this so that the bytes size is always the same 
 			b = b''
-			print("Recieved message of size {}b from {}".format(length, piAddr))
-			startTime = time.time()
+			print("{}\tRecieved message of size {}b from {}".format(round(time.time() - serverStartTime, 1), length, piAddr))
 			left = length
 			while left != 0:
 				batch = min(1024*1024, left)
 				newpart = connection.recv(batch)
 				left -= len(newpart)
 				b += newpart
-				print("{}b out of {}b: {}%".format(len(b),length,round((len(b)/length)*10000)/100))
-			# print("download finished, took {} seconds".format(time.time() - startTime))
+				print("{}\t{}b out of {}b: {}%".format(round(time.time() - serverStartTime, 1), len(b),length,round((len(b)/length)*10000)/100))
 			i = 0
 			sent = False
-			while i < len(connectedPCs):
-				c = connectedPCs[i]
-				if c[0].replace("\n","") == piId.replace("\n",""): # if they have the same ID
-					try:
-						l = str(len(b) + 10000000) # add this so that the string is always the same size
-						print("Sending message of size {}b to {}".format(len(b), c[0]))
-						c[1].send(l.encode("utf-8"))
-						c[1].sendall(b)
-						sent = True
-						break
-					except: # this means that the PC was disconnected 
-						# delete this PC from the list and try another computer
-						print()
-						print(traceback.format_exc())
-						print("{} has disconnected.".format(connectedPCs[i][2]))
-						connectedPCs.pop(i)
-						continue
-				i+=1
-			if not sent:
-				print("There are no connected PC's with the id [{}].".format(piId))
+			if len(connectedPCs) == 0:
+				print("{}\tNo connected PC's.".format(round(time.time() - serverStartTime, 1)))
+			else:
+				while i < len(connectedPCs):
+					c = connectedPCs[i]
+					if c[0].replace("\n","") == piId.replace("\n","") or len(connectedPCs) == 1: # if they have the same ID or there is only one connected PC
+						try:
+							l = str(len(b) + 10000000) # add this so that the string is always the same size
+							print("{}\tSending message of size {}b to {}".format(round(time.time() - serverStartTime, 1), len(b), c[0]))
+							c[1].send(l.encode("utf-8"))
+							c[1].sendall(b)
+							sent = True
+							break
+						except: # this means that the PC was disconnected 
+							# delete this PC from the list and try another computer
+							print()
+							print(traceback.format_exc())
+							print("{}\t{} has disconnected.".format(round(time.time() - serverStartTime, 1), connectedPCs[i][2]))
+							connectedPCs.pop(i)
+							continue
+					i+=1
+				if not sent:
+					print("{}\tThere are no connected PC's with the id [{}].".format(round(time.time() - serverStartTime, 1), piId))
+					print("{}\tConnected PC's are:\n\t\t{}".format(round(time.time() - serverStartTime, 1), connectedPCs))
 
 
 				
 	except Exception as e:
 		print(e)
-		print("Disconnecting pi",piId)
-
-
+		print("{}\tDisconnecting pi {}".format(round(time.time() - serverStartTime, 1), piId))
 
 
 def main():
-	print("Starting server")
+	time.sleep(1)
+	print("{}\tStarting server".format(round(time.time() - serverStartTime, 1)))
 	global connectedPCs
 	global connectedPIs
 	threads = []
@@ -110,38 +90,21 @@ def main():
 		# Establish connection with client. 
 		connection, new_addr = s.accept()
 		
-		print('Got connection from', new_addr)
+		print('{}\tGot connection from {}'.format(round(time.time() - serverStartTime, 1), new_addr))
 		machineType = connection.recv(1024).decode('utf-8') # this should either be PC or PI
 		parts = machineType.split(",")
-		print(parts)
 		if parts[0] == 'PC':
-			print("Its a PC")
+			print("{}\tIts a PC".format(round(time.time() - serverStartTime, 1)))
 			connectedPCs.append([parts[1], connection, new_addr])
 		elif parts[0] == 'PI':
-			print("Its a PI")
+			print("{}\tIts a PI".format(round(time.time() - serverStartTime, 1)))
 			connectedPIs.append([connection, new_addr])
 			t = threading.Thread(target=handlePI, args=(connection,new_addr,)) 
 			t.start() 
 			threads.append(t)
 		else:
-			print("Not a valid machine type")
+			print("{}\tNot a valid machine type".format(round(time.time() - serverStartTime, 1)))
 
 		
-
-
 if __name__ == '__main__':
 	main()
-
-
-
-	# print("trying to connect")
-	# try: # connect to PC
-	# 	ssh = paramiko.SSHClient()
-	# 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
-	# 	ssh.connect("192.168.1.240","22","jhala","Tigerandzues")
-	# 	sftp = ssh.open_sftp()
-	# except: # this will fail if the PC is not in the same network. 
-	# 	print("Could not connect")
-	# 	quit()
-
-	# print("connected")
